e05471c180771070d3a2793c70cf79e024f30404
JohnOlsea <91258573+JohnOlsea@users.noreply.github.com>
2023-04-26 19:32:08 +0700

    edituserprofile update

--- edituserprofile.py
+++ edituserprofile.py
@@ -12,14 +12,18 @@ firebase = pyrebase.initialize_app(firebaseConfig)
 # Connect to the Firebase Realtime Database
 db = firebase.database()
 
-def edit_item(username, email, firstname, lastname, dob,pos,pho):
+def edit_item(username, email, firstname, lastname, dob,pos,pho,gender,edu,epr,sk):
     data = {
         "email": email,
         "firstname": firstname,
         "lastname": lastname,
         "dob": dob,
         "position":pos,
-        "phone_number":pho
+        "phone_number":pho,
+        "gender":gender,
+        "education":edu,
+        "experience":epr,
+        "skills":sk
     }
     # db.child("users").push(data)
     db.child("users").child(username).update(data)
@@ -44,17 +48,39 @@ class UserUI(QWidget):
         self.ui.emailedit.setText(db.child("users").child(arg1).child("email").get().val())
         self.ui.phoneedit.setText(db.child("users").child(arg1).child("phone_number").get().val())
         self.ui.posedit.setText(db.child("users").child(arg1).child("position").get().val())
+        self.ui.eduedit.setText(db.child("users").child(arg1).child("education").get().val())
+        self.ui.epredit.setText(db.child("users").child(arg1).child("experience").get().val())
+        self.ui.skilledit.setText(db.child("users").child(arg1).child("skills").get().val())
+
+        if (db.child("users").child(arg1).child("gender").get().val()=="Male"):
+            self.ui.malebutton.setChecked(True)
+        elif (db.child("users").child(arg1).child("gender").get().val() == "Female"):
+            self.ui.femalebutton.setChecked(True)
+        elif (db.child("users").child(arg1).child("gender").get().val() == "Prefer not to say"):
+            self.ui.otherbutton.setChecked(True)
 
     def update(self):
         firstname = self.ui.firstedit.text()
         lastname = self.ui.lastedit.text()
         dob = self.ui.dobedit.text()
+        gender = ""
         email = self.ui.emailedit.text()
         pos= self.ui.posedit.text()
-        pho=self.ui.phoneedit.text() 
+        pho=self.ui.phoneedit.text()
+        edu = self.ui.eduedit.toPlainText()
+        epr = self.ui.epredit.toPlainText()
+        sk =  self.ui.skilledit.toPlainText()
         username= arg1
 
-        if(edit_item(username, email, firstname, lastname, dob,pos,pho)):
+
+        if(self.ui.malebutton.isChecked()):
+            gender = "Male"
+        elif (self.ui.femalebutton.isChecked()):
+            gender = "Female"
+        elif (self.ui.otherbutton.isChecked()):
+            gender = "Prefer not to say"
+
+        if(edit_item(username, email, firstname, lastname, dob,pos,pho,gender,edu,epr,sk)):
             QMessageBox.information(self,"Success", f"update successful!")
         else:
             QMessageBox.information(self,"ERROR", f"update failed.")
